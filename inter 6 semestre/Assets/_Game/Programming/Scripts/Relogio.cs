f8a4996180575ff721d43ec8d1e77077b7ad6a6b
lucaastrigo <lucaas.trigo@gmail.com>
2021-11-25 19:34:48 -0300

    inteeeeeeeeeer;

--- inter 6 semestre/Assets/_Game/Programming/Scripts/Relogio.cs
+++ inter 6 semestre/Assets/_Game/Programming/Scripts/Relogio.cs
@@ -11,6 +11,11 @@ public class Relogio : MonoBehaviour
     public RectTransform relogioSeta;
     public GameObject agradecimentos;
 
+    public GameObject[] finalDesligado;
+    public GameObject[] finalLigado;
+
+    bool final;
+
     private void Start()
     {
         actions = 16;
@@ -23,6 +28,24 @@ public class Relogio : MonoBehaviour
 
         float rotacaoSeta = -360 + (float)actions * 22.5f; //era * 90
         relogioSeta.eulerAngles = new Vector3(0, 0, rotacaoSeta);
+
+
+
+
+        if(actions == 0 && !final)
+        {
+            for (int i = 0; i < finalDesligado.Length; i++)
+            {
+                finalDesligado[i].SetActive(false);
+            }
+
+            for (int i = 0; i < finalLigado.Length; i++)
+            {
+                finalLigado[i].SetActive(true);
+            }
+
+            final = true;
+        }
     }
 
     public void Agradece()
